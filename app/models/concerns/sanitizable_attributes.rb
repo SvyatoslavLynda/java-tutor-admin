70fd6bb6779f1967eb6d6759f67bb88eeb74b19d
Svyatoslav Lynda <svyatoslavlynda@gmail.com>
2016-06-01 10:51:04 +0300

    fixed images

--- app/models/concerns/sanitizable_attributes.rb
+++ app/models/concerns/sanitizable_attributes.rb
@@ -9,7 +9,6 @@ module SanitizableAttributes
     )
     new_value = replace_tag(new_value, old: :strong, new: :b)
     new_value = replace_tag(new_value, old: :em, new: :i)
-    new_value = convert_img_sources(new_value)
     public_send "#{attr}=", new_value
   end
 
@@ -17,16 +16,5 @@ module SanitizableAttributes
 
   def replace_tag(html, old: , new:)
     html.gsub(/(<\/?)\s*#{old}\s*>/i, "\\1#{new}>")
-  end
-
-  def convert_img_sources(html)
-    html.gsub(/(?<=src\=")(.*?)(?=")/) {|url| generate_correct_img_src(url.to_s.strip)}
-  end
-
-  def generate_correct_img_src(url)
-    path = Addressable::URI.parse(url).try(:path)
-    return path if path.blank?
-    path.insert(0, '/') unless path.first == '/'
-    path
   end
 end
